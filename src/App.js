b8132facd23fe9f3b88d553e5f6f643710b696ed
Rajsekar Manokaran <rajsekar@gmail.com>
2017-11-02 01:43:59 +0530

    initial commit:  from create-react-app for web administration of p-tracking system

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <div className="App-title">
+            <h1>Participatory Tracking - Admin</h1>
+          </div>
+          <div>
+            <div>Surveys</div>
+            <div>Surveyors</div>
+          </div>
+        </header>
+      </div>
+    );
+  }
+}
+
+export default App;
