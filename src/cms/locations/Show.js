615fa99811d37c2c303b1be80e486d9c7956d0df
Rajsekar Manokaran <rajsekar@gmail.com>
2018-03-19 23:36:03 +0530

    + moved leaflet-webpack-hack to src/index.js

--- src/cms/locations/Show.js
+++ src/cms/locations/Show.js
@@ -3,14 +3,6 @@ import ShowPage from '../base/Show';
 import YAML from 'js-yaml';
 
 import L from 'leaflet';
-import 'leaflet/dist/leaflet.css';
-delete L.Icon.Default.prototype._getIconUrl;
-
-L.Icon.Default.mergeOptions({
-  iconRetinaUrl: require('leaflet/dist/images/marker-icon-2x.png'),
-  iconUrl: require('leaflet/dist/images/marker-icon.png'),
-  shadowUrl: require('leaflet/dist/images/marker-shadow.png'),
-});
 
 export default class Show extends ShowPage {
   render() {
--- src/index.js
+++ src/index.js
@@ -3,4 +3,14 @@ import ReactDOM from 'react-dom';
 import './index.scss';
 import App from './App';
 
+import L from 'leaflet';
+import 'leaflet/dist/leaflet.css';
+delete L.Icon.Default.prototype._getIconUrl;
+
+L.Icon.Default.mergeOptions({
+  iconRetinaUrl: require('leaflet/dist/images/marker-icon-2x.png'),
+  iconUrl: require('leaflet/dist/images/marker-icon.png'),
+  shadowUrl: require('leaflet/dist/images/marker-shadow.png'),
+});
+
 ReactDOM.render(<App />, document.getElementById('root'));
