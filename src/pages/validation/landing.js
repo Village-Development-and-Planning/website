fcbbf03160b8f79343e231e77324456243a5971e
Rajsekar Manokaran <rajsekar@gmail.com>
2018-06-15 14:29:58 +0530

    + minor style fixes

--- src/pages/validation/landing.js
+++ src/pages/validation/landing.js
@@ -1,11 +1,10 @@
 import React from 'react';
 import ValidationList from './List';
-import ActionPanel from '../../layout/ActionPanel';
-import Responsive from '../../layout/Responsive';
-import {T} from '../../translations';
-
-import home from '../../images/home.png';
-import validate from '../../images/validate.png';
+// import ActionPanel from '../../layout/ActionPanel';
+// import Responsive from '../../layout/Responsive';
+// import {T} from '../../translations';
+// import home from '../../images/home.png';
+// import validate from '../../images/validate.png';
 
 export default (props) => <React.Fragment>
   <ValidationList
